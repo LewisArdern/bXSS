a3a4407d03b6d220f92f24ab1b07b1b3465b6740
inkz <inkz@xakep.ru>
2019-06-01 17:33:45 +1000

    Add configurable body-parser limit

--- server/config/express.js
+++ server/config/express.js
@@ -1,11 +1,13 @@
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const config = require('../utilities/config');
 
 module.exports = app => {
   app.use(helmet()); // Remove x-powered and add default security headers
-  app.use(bodyParser.json()); // to support JSON-encoded bodies
+  app.use(bodyParser.json({ limit: config.bodyLimit })); // to support JSON-encoded bodies
   app.use(
     bodyParser.urlencoded({
+      limit: config.bodyLimit,
       // to support URL-encoded bodies
       extended: true
     })
